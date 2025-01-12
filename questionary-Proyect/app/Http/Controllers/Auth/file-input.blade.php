2d8765f9ec1f3c6706f859f115f57febad97a0ea
leandro <leandro@cyberargsistemas.com.ar>
2025-01-12 17:34:48 -0300

    navbar completado ,se agrego campos username y picture_user

---
+++ questionary-Proyect/app/Http/Controllers/Auth/file-input.blade.php
@@ -0,0 +1,2 @@
+<!-- resources/views/components/file-input.blade.php -->
+<input type="file" name="{{ $attributes['name'] }}" id="{{ $attributes['id'] }}" class="block mt-1 w-full" {{ $attributes }} />
