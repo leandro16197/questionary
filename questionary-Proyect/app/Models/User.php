d0bd8a6bcc6a36c21701141bc15f6b73325786d7
leandro <leandro@cyberargsistemas.com.ar>
2025-01-14 18:42:59 -0300

    add created UserAdminSeeder

--- questionary-Proyect/app/Models/User.php
+++ questionary-Proyect/app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name', 'email', 'password', 'username', 'profile_picture',
+        'name', 'email', 'password','rol', 'username', 
     ];
     /**
      * The attributes that should be hidden for serialization.
